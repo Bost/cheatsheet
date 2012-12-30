6bdd140402c5b725297f660993dbe70fb13b9a30
bambi-small <thebost@gmail.com>
2012-12-30 10:23:12 +0100

    interface borders

---
+++ patterns/LogTest.java
@@ -0,0 +1,32 @@
+    public class LogTest {
+        private Logger logger;
+
+        @Before
+        public void initialize() {
+            logger = Logger.geetLogger("logger");
+            logger.removeAllAppenders();
+            logger.getRootLogger().removeAllAppenders();
+        }
+
+        @Test
+        public void basicLogger() {
+            BasicConfigurator.configure();
+            logger.info("basicLogger");
+        }
+
+        @Test
+        public void addAppenderWithStream() {
+            logger.addAppender(new ConsoleAppender(
+                new PatternLayout("%p %t %m%n"),
+                ConsoleAppender.SYSTEM_OUT));
+            logger.info("addAppenderWithStream");
+        }
+
+        @Test
+        public void addAppenderWithoutStream() {
+            logger.addAppender(new ConsoleAppender(
+                new PatternLayout("%p %t %m%n")));
+            logger.info("addAppenderWithoutStream");
+        }
+    }
+
