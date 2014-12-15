b8d106615e1c7f24ad9ded994651c4ee80ef3ac1
Bost <thebost@gmail.com>
2014-12-15 13:30:47 +0100

    rest-commands.js: typo

--- rest-commands.js
+++ rest-commands.js
@@ -150,7 +150,7 @@ var jsonRest = [
 ['net use P: \\\\server\\path',                                                    '# windows: net: map P: as a network drive for \\\\sever\\path'],
 ['net use P: /delete',                                                             '# windows: net: unmap P:'],
 ['netstat -a | find "LISTENING"',                                                  '# windows: open ports'],
-['setx NAME value /m',                                                             '# widows: set system variable system-wide'],
+['setx NAME value /m',                                                             '# windows: set system variable system-wide'],
 
 ['netstat -lnptu',                                                                 '# linux: open ports'],
 ['db2cmd -i -w db2clpsetcp',                                                       '# db2: init environment'],
