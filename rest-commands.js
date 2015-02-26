020275d5b1cc1f3d4ea8d765f015342bbdcae963
Bost <thebost@gmail.com>
2015-02-26 17:43:47 +0100

    rest-commands.js: evince file.pdf

--- rest-commands.js
+++ rest-commands.js
@@ -1,5 +1,6 @@
 var jsonRest = [
 //['', '# '],
+['evince file.pdf',                               '# pdf: view file.pdf'],
 ['sudo nethogs wlan0',                            '# net: grouping bandwidth per process; "net top"'],
 ['wget http://ftp.heanet.ie/mirrors/gnu/gnu-keyring.gpg && gpg --import gnu-keyring.gpg', '# gpg: download and import gnu-keyring'],
 ['gpg --verify file.sig file',                    '# gpg: verify file'],
