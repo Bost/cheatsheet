7dcb3045cfe93bda26fabc91c1e9d3b6df4d4e8c
Bost <thebost@gmail.com>
2015-03-09 13:45:52 +0100

    rest-commands.js: site - ftp: sends site specific commands to remote server

--- rest-commands.js
+++ rest-commands.js
@@ -289,76 +289,33 @@ var jsonRest = [
 
 ['wireshark',                                     '# net: packet analyser'],
 ['get',                                           '# ftp:'],
+['site',                                          '# ftp: sends site specific commands to remote server'],
 ['sysinfo',                                       '# System Information Extraction Program:'],
 /*
 ['',                                           '# :'],
-* TODO disable mysql daemon from autostart
-
 * TODO add this:
 git remote add --track develop upstream https://github.com/Lokaltog/powerline.git
 
-#!/bin/sh
-ls -lh &&
-# This is a comment
-echo 'Wicked, it works!'
-
 * TODO add bash file tests to the list
-FILE1 -ot FILE2
-	      FILE1 is older than FILE2
-
-       -b FILE
-	      FILE exists and is block special
-
-       -c FILE
-	      FILE exists and is character special
-
-       -d FILE
-	      FILE exists and is a directory
-
-       -e FILE
-	      FILE exists
-
-       -f FILE
-	      FILE exists and is a regular file
-
-       -g FILE
-	      FILE exists and is set-group-ID
-
-       -G FILE
-	      FILE exists and is owned by the effective group ID
-
-       -h FILE
-	      FILE exists and is a symbolic link (same as -L)
-
-       -k FILE
-	      FILE exists and has its sticky bit set
-
-       -L FILE
-	      FILE exists and is a symbolic link (same as -h)
-
-       -O FILE
-	      FILE exists and is owned by the effective user ID
-
-       -p FILE
-	      FILE exists and is a named pipe
-
-       -r FILE
-	      FILE exists and read permission is granted
-       -s FILE
-	      FILE exists and has a size greater than zero
-
-       -S FILE
-	      FILE exists and is a socket
-
-       -t FD  file descriptor FD is opened on a terminal
-
-       -u FILE
-	      FILE exists and its set-user-ID bit is set
-
-       -w FILE
-	      FILE exists and write permission is granted
-
-       -x FILE
-	      FILE exists and execute (or search) permission is granted
+FILE1 -ot FILE2: FILE1 is older than FILE2
+       -b FILE:  FILE exists and is block special
+       -c FILE:  FILE exists and is character special
+       -d FILE:  FILE exists and is a directory
+       -e FILE:  FILE exists
+       -f FILE:  FILE exists and is a regular file
+       -g FILE:  FILE exists and is set-group-ID
+       -G FILE:  FILE exists and is owned by the effective group ID
+       -h FILE:  FILE exists and is a symbolic link (same as -L)
+       -k FILE:  FILE exists and has its sticky bit set
+       -L FILE:  FILE exists and is a symbolic link (same as -h)
+       -O FILE:  FILE exists and is owned by the effective user ID
+       -p FILE:  FILE exists and is a named pipe
+       -r FILE:  FILE exists and read permission is granted
+       -s FILE:  FILE exists and has a size greater than zero
+       -S FILE:  FILE exists and is a socket
+       -t FD:    file descriptor FD is opened on a terminal
+       -u FILE:  FILE exists and its set-user-ID bit is set
+       -w FILE:  FILE exists and write permission is granted
+       -x FILE:  FILE exists and execute (or search) permission is granted
 */
  ]
