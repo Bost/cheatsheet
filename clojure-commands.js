33ac17f2d258d1ca334f1dfc4d6e343b8c49ac0e
Bost <thebost@gmail.com>
2015-04-02 16:43:22 +0200

    clojure-commands.js: use cider-refresh to clean the REPL

--- clojure-commands.js
+++ clojure-commands.js
@@ -1,8 +1,8 @@
 var jsonRest = [
 //['', '; '],
-['(keys (ns-publics \'ws.core))',                                       '; clj: show objects in the namespace'],
-['(map #(ns-unmap *ns* %) (keys (ns-interns *ns*)))',                   '; clj: clean the repl'],
-['(load-file "src/ws/core.clj")',                                       '; clj: read and eval swc/ws/core.clj'],
-['(in-ns \'full.namespace)',                                            '; clj: switch to full.namespace'],
-['(clojure.repl/doc full.namespace/objname)',                           '; clj: objname documentation'],
+['(keys (ns-publics \'ws.core))',                     '; clj: show objects in the namespace'],
+['(map #(ns-unmap *ns* %) (keys (ns-interns *ns*)))', '; clj: should clean the repl; use cider-refresh'],
+['(load-file "src/ws/core.clj")',                     '; clj: read and eval swc/ws/core.clj'],
+['(in-ns \'full.namespace)',                          '; clj: switch to full.namespace'],
+['(clojure.repl/doc full.namespace/objname)',         '; clj: objname documentation'],
  ]
