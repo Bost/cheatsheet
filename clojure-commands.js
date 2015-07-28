16528ed8063b8b62be68bfa178bf81c34ee0311c
Bost <thebost@gmail.com>
2015-07-28 10:54:42 +0200

    show objects in the namespace

--- clojure-commands.js
+++ clojure-commands.js
@@ -1,6 +1,6 @@
 var jsonRest = [
 //['', '; '],
-['(keys (ns-publics \'ws.core))',                     '; clj: show objects in the namespace'],
+['(sort (keys (ns-publics \'ws.core)))',              '; clj: show objects in the namespace'],
 ['(map #(ns-unmap *ns* %) (keys (ns-interns *ns*)))', '; clj: should clean the repl; use cider-refresh'],
 ['(load-file "src/ws/core.clj")',                     '; clj: read and eval swc/ws/core.clj'],
 ['(in-ns \'full.namespace)',                          '; clj: switch to full.namespace'],
