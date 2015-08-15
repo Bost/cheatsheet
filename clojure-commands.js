462e7e716366d3f61a00d6415917571efe6b654b
Bost <thebost@gmail.com>
2015-08-15 20:27:34 +0200

    cider: usefull functions and shortcuts

--- clojure-commands.js
+++ clojure-commands.js
@@ -9,5 +9,10 @@ var jsonRest = [
 ['rlwrap lein deps :tree',                                              '; clj: show leiningen dependency tree'],
 ['cd $LIGHTTABLE/deploy/plugins/ && lein new lt-plugin my-plugin',      '; clj: create lighttable plugin'],
 ['[the-dependency "X.Y.Z" :exclusions [org.clojure/clojure]]',          '; clj: try to put it to project.clj in case of \'Could not locate clojure/instant__init.class or clojure/instant.clj on classpath\''],
-['cljr-rename-symbol' , 'clj: refactoring'],
+['C-c RET r s',        'clj: refactoring: M-x cljr-rename-symbol'],
+['C-c M-o',            'clj: M-x cider-repl-clear-buffer'],
+['C-c C-o',            'clj: M-x cider-repl-clear-output'],
+['C-c C-d C-d',        'clj: M-x cider-doc'],
+['C-c C-p',            'clj: M-x cider-pprint-eval-last-sexp']
+['C-c C-f',            'clj: M-x cider-pprint-eval-defun-at-point']
  ]
