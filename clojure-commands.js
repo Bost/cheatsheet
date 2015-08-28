ea123c2ad6a62ca7e683a79cd4922349f761815c
Bost <thebost@gmail.com>
2015-08-28 12:10:07 +0200

    Readability

--- clojure-commands.js
+++ clojure-commands.js
@@ -9,7 +9,7 @@ var jsonRest = [
 ['rlwrap lein deps :tree',                                              '; clj: show leiningen dependency tree'],
 ['cd $LIGHTTABLE/deploy/plugins/ && lein new lt-plugin my-plugin',      '; clj: create lighttable plugin'],
 ['[the-dependency "X.Y.Z" :exclusions [org.clojure/clojure]]',          '; clj: try to put it to project.clj in case of \'Could not locate clojure/instant__init.class or clojure/instant.clj on classpath\''],
-['C-c RET r s',        'clj: refactoring: M-x cljr-rename-symbol'],
+['C-c RET r s',        'clj: M-x cljr-rename-symbol refactoring:'],
 ['C-c M-o',            'clj: M-x cider-repl-clear-buffer'],
 ['C-c C-o',            'clj: M-x cider-repl-clear-output'],
 ['C-c C-d C-d',        'clj: M-x cider-doc'],
