4243621d7eede203c0214c6fa29817a95e1fb475
Bost <thebost@gmail.com>
2016-12-10 11:27:54 +0100

    lein -o; cleanup

--- commands-clojure.clj
+++ commands-clojure.clj
@@ -3,74 +3,74 @@
 ;; clojure.spec - examples
 (clojure.spec/exercise [spec] [spec n] [spec n overrides])
 
-;; clj: show objects in the namespace
+;; show objects in the namespace
 (sort (keys (ns-publics \'ws.core)))
 
-;; clj: should clean the repl; use cider-refresh
+;; should clean the repl; use cider-refresh
 (map #(ns-unmap *ns* %) (keys (ns-interns *ns*)))
 
-;; clj: read and eval swc/ws/core.clj
+;; read and eval swc/ws/core.clj
 (load-file "src/ws/core.clj")
 
-;; clj: switch to full.namespace
+;; switch to full.namespace
 (in-ns 'full.namespace)
 
-;; clj: Returns a sequence of all namespaces.
+;; Returns a sequence of all namespaces.
 (all-ns)
 
-;; clj: objname documentation / M-x cider-doc (C-c C-d C-d) / M-x cider-javadoc
+;; objname documentation / M-x cider-doc (C-c C-d C-d) / M-x cider-javadoc
 (clojure.repl/doc full.namespace/objname)
 
-;; clj: block comment; NOTE: (comment) returns null
+;; block comment; NOTE: (comment) returns null
 
 #_(foo 1 2)/ (comment foo 1 2)
 
 ;; print stack trace: (/ 1 0) (pst)
 (pst)
 
-;; clj: show leiningen dependency tree / classpath
+;; show leiningen dependency tree / classpath
 ;; lein deps :tree / lein classpath
 
-;; clj: create lighttable plugin
+;; leiningen : Run a task offline.
+lein -o
+
+;; create lighttable plugin
 ;; cd $LIGHTTABLE/deploy/plugins/ && lein new lt-plugin my-plugin
 
-;; clj: try to put it to project.clj in case of \'Could not locate clojure/instant__init.class or clojure/instant.clj on classpath\'
+;; try to put it to project.clj in case of \'Could not locate clojure/instant__init.class or clojure/instant.clj on classpath\'
 ;; [the-dependency "X.Y.Z" :exclusions [org.clojure/clojure]]
 
-;; clj: M-x cljr-rename-symbol refactoring:
+;; M-x cljr-rename-symbol refactoring:
 ;; C-c RET r s
 
-;; clj: M-x cider-repl-clear-buffer
+;; M-x cider-repl-clear-buffer
 ;; C-c M-o
 
-;; clj: M-x cider-repl-clear-output
+;; M-x cider-repl-clear-output
 ;; C-c C-o
 
-;; clj: M-x cider-doc
+;; M-x cider-doc
 ;; C-c C-d C-d
 
-;; clj: M-x cider-pprint-eval-last-sexp
+;; M-x cider-pprint-eval-last-sexp
 ;; C-c C-p
 
-;; clj: M-x cider-pprint-eval-defun-at-point
+;; M-x cider-pprint-eval-defun-at-point
 ;; C-c C-f
 
-;; clj: M-x cider-refresh: reload all modified Clojure files on the classpath
+;; M-x cider-refresh: reload all modified Clojure files on the classpath
 ;; C-c C-x
 
-;; clj: C-M-x cider-eval-defun-at-point
+;; C-M-x cider-eval-defun-at-point
 ;; C-u C-M-x cider-debug
 
-;; clj: Transpose matrix
+;; Transpose matrix
 (apply map vector [[1 2] [3 4]]) ;;=> ([1 3] [2 4])
-
-;; clj: install boot
-sudo bash -c "cd /usr/local/bin && curl -fsSLo boot https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh && chmod 755 boot"
 
 ;; clojure.core.typed
 (t/check-ns)
 
-;;
+;; memoization
 (clojure.core.memoize/memo-clear! f args)
 
 ;; Type Hints: http://clojure.org/reference/java_interop#typehints
