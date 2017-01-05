f8f5ebb1022fb6a9d480bf24146059d4cf160302
Bost <thebost@gmail.com>
2017-01-06 00:09:18 +0100

    figwheel controls

--- commands-clojure.clj
+++ commands-clojure.clj
@@ -122,3 +122,14 @@ lein repl :headless :host 0.0.0.0 :port <portNr>
 
 ;; reader conditionals - for different platforms
 #?(:clj Double/NaN :cljs js/NaN :default nil)
+
+;; Figwheel Controls:
+(stop-autobuild)            ;; stops Figwheel autobuilder
+(start-autobuild [id ...])  ;; starts autobuilder focused on optional ids
+(switch-to-build id ...)    ;; switches autobuilder to different build
+(reset-autobuild)           ;; stops, cleans, and starts autobuilder
+(reload-config)             ;; reloads build config and resets autobuild
+(build-once [id ...])       ;; builds source one time
+(clean-builds [id ..])      ;; deletes compiled cljs target files
+(print-config [id ...])     ;; prints out build configurations
+(fig-status)                ;; displays current state of system
