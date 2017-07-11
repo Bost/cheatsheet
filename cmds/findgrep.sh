83e5cf0ff03f6f1f2273e9c4be1dcaca5d3f3318
Bost <thebost@gmail.com>
2017-07-11 14:34:16 +0200

    Fix missing '--'

--- cmds/findgrep.sh
+++ cmds/findgrep.sh
@@ -41,13 +41,13 @@ find . -type f -name "*.xml" -or -name "*.txt"
 find . -executable -type f
 
 # grep: find: recursive search for "String" in ... (with '.' at the end)
-grep -nir "String" exclude-dir={.git,CVS} include=\*.{el,clj,cljs,cljc} ./
+grep -nir "String" --exclude-dir={.git,CVS} --include=\*.{el,clj,cljs,cljc} ./
 
 # grep: find: grep-help: recursive search for "String" in ... (with '.' at the end)
-grep -nir "String" exclude-dir={.git,CVS} include=\*.{log,propeties,cfg,txt} ./
+grep -nir "String" --exclude-dir={.git,CVS} --include=\*.{log,propeties,cfg,txt} ./
 
 # grep: search for "String" in *.txt files (with spaces in filenames)
-find ./ -type f -name "*.txt" -print0 | xargs -0 grep -l "String"
+find ./ -type f -name "*.txt" -print0 | xargs -0 grep --files-with-matches "String"
 
 # find and delete *.jar and *.class when idling
 ionice -c3 find . -name "*.jar" -or -name "*.class" -delete
@@ -65,7 +65,7 @@ find . -type f ! -name "*~" -exec grep -nH -e "String" {} +
 find . -name *.properties -exec grep -lir ".*String.*" \'{}\' \; -print
 
 # grep: colorize grep in less
-grep color=always pattern file | less -R
+grep --color=always pattern file | less -R
 
 # grep: lines containing any upper character
 grep "[[:upper:]]" file
@@ -74,19 +74,19 @@ grep "[[:upper:]]" file
 grep -Fx -f file1 file2
 
 # grep: search for "String" and print 2/4 lines before/after matching line
-grep -B 2 -A 4 "String" / grep before-context=2 after-context=4 "String"
+grep -B 2 -A 4 "String" / grep --before-context=2 --after-context=4 "String"
 
 # grep: print only filenames of the files containing "String"
 grep "String" * | cut -f1 -d:
 
 # grep: find: recursive search for "String" in ... (with '.' at the end)
-grep -nir "String" exclude-dir={.git,CVS} include=\*.{el,clj,cljs,cljc} ./
+grep -nir "String" --exclude-dir={.git,CVS} --include=\*.{el,clj,cljs,cljc} ./
 
 # grep: find: grep-help: recursive search for "String" in ... (with '.' at the end)
-grep -nir "String" exclude-dir={.git,CVS} include=\*.{log,propeties,cfg,txt} ./
+grep -nir "String" --exclude-dir={.git,CVS} --include=\*.{log,propeties,cfg,txt} ./
 
 # grep: search for "String" in *.txt files (with spaces in filenames)
-find ./ -type f -name "*.txt" -print0 | xargs -0 grep -l "String"
+find ./ -type f -name "*.txt" -print0 | xargs -0 grep --files-with-matches "String"
 
 # zgrep: search possibly compressed files for a regular expression
 zgrep "String" myfile.gz / zgrep 'GET /blog' access_log.gz
