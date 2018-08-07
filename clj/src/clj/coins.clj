8bcdcf88633523f1d63e5e444fadb08e014a2bcf
Bost <thebost@gmail.com>
2018-08-07 13:15:04 +0200

    hyperleddger

---
+++ clj/src/clj/coins.clj
@@ -0,0 +1,6 @@
+(ns clj.coins)
+
+;; hyperleddger: open-source project developed by the Linux foundation. It uses
+;; a modular approach to building blockchains collective of open-source
+;; blockchains and tools that anyone can use to create their own distributed
+;; ledgers.
