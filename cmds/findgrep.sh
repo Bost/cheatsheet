e64e5e052b0371b7131f1d4bbe63f8d0eebf1c2f
Bost <thebost@gmail.com>
2019-05-28 15:05:15 +0200

    grep: \Z  matches the EOF end-of-file

--- cmds/findgrep.sh
+++ cmds/findgrep.sh
@@ -114,3 +114,6 @@ grep -nir -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" --exclude-dir={.git} --include=\
 
 # match integer / floating point number
 echo "aaa 123 0.0 bbb" | grep --only-matching '\([[:digit:]]*\.[[:digit:]]*\|[[:digit:]]*\)'
+
+# grep
+# \Z  matches the EOF end-of-file
