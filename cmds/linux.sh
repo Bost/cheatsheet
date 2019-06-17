d45d68623be08b7147edda43c104aee17abf4339
Bost <thebost@gmail.com>
2019-06-17 18:31:41 +0200

    More docker commands

--- cmds/linux.sh
+++ cmds/linux.sh
@@ -1247,9 +1247,14 @@ chown -h myuser:mygroup mysymbolic
 # Stich ETL service https://www.stitchdata.com/
 # https://clojure.org/stories/stitch
 
-# cgroups - control groups in the linux kernel
-sudo docker tag friendlyhello gordon/get-started:part2
+# cgroups - control groups in the linux kernel; limits an application to a
+# specific set of resources
+# control groups allow Docker Engine to share available hardware resources to
+# containers and optionally enforce limits and constraints. For example, you can
+# limit the memory available to a specific container.
 sudo docker run -p 4000:80 friendlyhell
+sudo docker tag friendlyhello gordon/get-started:part2
+sudo docker run -p 4000:80 gordon/get-started:part2
 sudo docker image ls
 sudo docker save gordon/get-started:part2 > get-started.tar
 sudo docker load --image get-started.tar
@@ -1260,7 +1265,51 @@ sudo docker kill --signal=SIGHUP <container>
 sudo docker stop <container>
 sudo docker restart <container>
 sudo systemctl restart docker
+sudo service docker status
 sudo docker ps -a
 sudo killall docker-containerd
 sudo killall dockerd
 sudo docker-compose down
+
+# services
+sudo docker swarm init
+sudo docker stack deploy -c docker-compose.yml getstartedlab
+sudo docker service ls
+sudo docker stack ls
+sudo docker stack services getstartedlab
+sudo docker service ps getstartedlab_web
+sudo docker container ls -q
+
+# https://docs.docker.com/get-started/part2/
+docker build -t friendlyhello .  # Create image using this directory's Dockerfile
+docker run -p 4000:80 friendlyhello  # Run "friendlyhello" mapping port 4000 to 80
+docker run -d -p 4000:80 friendlyhello         # Same thing, but in detached mode
+docker container ls                                # List all running containers
+docker container ls -a             # List all containers, even those not running
+docker container stop <hash>           # Gracefully stop the specified container
+docker container kill <hash>         # Force shutdown of the specified container
+docker container rm <hash>        # Remove specified container from this machine
+docker container rm $(docker container ls -a -q)         # Remove all containers
+docker image ls -a                             # List all images on this machine
+docker image rm <image id>            # Remove specified image from this machine
+docker image rm $(docker image ls -a -q)   # Remove all images from this machine
+docker login             # Log in this CLI session using your Docker credentials
+docker tag <image> username/repository:tag  # Tag <image> for upload to registry
+docker push username/repository:tag            # Upload tagged image to registry
+docker run username/repository:tag                   # Run image from a registry
+
+# scale the app by changing the replicas value in docker-compose.yml
+docker stack deploy -c docker-compose.yml getstartedlab
+sudo docker stack rm getstartedlab
+sudo docker swarm leave --force
+
+# https://docs.docker.com/get-started/part3/
+docker stack ls                                            # List stacks or apps
+docker stack deploy -c <composefile> <appname>  # Run the specified Compose file
+docker service ls                 # List running services associated with an app
+docker service ps <service>                  # List tasks associated with an app
+docker inspect <task or container>                   # Inspect task or container
+docker container ls -q                                      # List container IDs
+docker stack rm <appname>                             # Tear down an application
+docker swarm leave --force      # Take down a single node swarm from the manager
+
